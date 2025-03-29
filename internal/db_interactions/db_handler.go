e497f1ca67228310345116b3aa62d63dc4202068
lachlan <lachikent@gmail.com>
2025-03-29 15:39:57 +1000

    add untested functions for adding and removing recipies

--- internal/db_interactions/db_handler.go
+++ internal/db_interactions/db_handler.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"errors"
 	"os"
+	"slices"
 
 	"github.com/lachlancd/cocktail_menu/internal/models"
 )
@@ -20,5 +21,44 @@ func ReadRecipeJson() (*[]models.Recipe, error) {
 		return nil, errors.New("Error parsing recipes")
 	}
 
-  return &recipeCollection, nil
+	return &recipeCollection, nil
+}
+
+func AddRecipeJson(newRecipe models.Recipe) error {
+	var recipeCollection, err = ReadRecipeJson()
+	if err != nil {
+		return err
+	}
+
+	*recipeCollection = append(*recipeCollection, newRecipe)
+
+	data, err := json.Marshal(recipeCollection)
+	if err != nil {
+		return errors.New("Error adding new recipe to list")
+	}
+
+	if err := os.WriteFile("internal/data/recipes.json", data, 0666); err != nil {
+		return errors.New("Error adding new recipe to list")
+	}
+
+	return nil
+}
+
+func DeleteRecipeJson(recipe models.Recipe) error {
+	var recipeCollection, err = ReadRecipeJson()
+	if err != nil {
+		return err
+	}
+
+	if recipe.Index > len(*recipeCollection) {
+		return errors.New("recipe index out of range")
+	}
+
+  *recipeCollection = slices.Delete(*recipeCollection, recipe.Index, recipe.Index + 1)
+
+  for _, r := range (*recipeCollection)[recipe.Index:] {
+    r.Index -= 1
+  }
+
+	return nil
 }
